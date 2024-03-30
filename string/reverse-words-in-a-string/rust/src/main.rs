e1b885dfccc0ceb43125e07c07466f462767f0db
James Chen <jamesc@swann.com>
2024-03-30 21:26:10 +1100

    Rust: Reverse the words in a string

---
+++ string/reverse-words-in-a-string/rust/src/main.rs
@@ -0,0 +1,17 @@
+fn reverse_words_in_a_string(input: String) -> String
+{
+    let mut v: Vec<String> = Vec::new();
+    for i in input.split_whitespace() {
+        v.push(i.to_string());
+    }
+    v.reverse();
+    v.join(" ")
+}
+
+fn main() {
+    let output1 = reverse_words_in_a_string("Hello, world!".to_string());
+    println!("{}", output1);
+
+    let output2 = reverse_words_in_a_string("bashful doc dopey grumpy happy sleepy sneezy".to_string());
+    println!("{}", output2);
+}
