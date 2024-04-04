f64e81f1d29dfd6f6aa758a6f85aafa00ede5e74
James Chen <jamesc@swann.com>
2024-04-05 08:30:49 +1100

    C++: Print k largest elements in array fixes

--- array/k-largest-elements/c++/main.cpp
+++ array/k-largest-elements/c++/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iterator>
 #include <set>
 #include <vector>
 
@@ -6,6 +7,22 @@ using namespace std;
 
 void print_k_largest_elements(const vector<int> &input, int k)
 {
+    cout << "Print " << k << " largest elements in " << endl;
+    copy(input.begin(), input.end(), ostream_iterator<int>(cout, ", "));
+    cout << endl;
+
+    if (k < 0) {
+        cerr << "Invalid input k: " << k << "\n" << endl;
+        return;
+    }
+
+    if (k > (int)input.size()) {
+        cout << "k: " << k;
+        k = input.size();
+        cout << " is greater than array size: " << input.size() << endl;
+        cout << "print all elements" << endl;
+    }
+
     set<int, greater<int>> s(input.begin(), input.end());
 
     auto i = s.begin();
@@ -16,12 +33,14 @@ void print_k_largest_elements(const vector<int> &input, int k)
         --k;
     }
     
-    cout << endl;
+    cout << "\n" << endl;
 }
 
 
 int main()
 {
     vector<int> input{ 1, 23, 12, 9, 30, 2, 50 };
+    print_k_largest_elements(input, -1);
+    print_k_largest_elements(input, 20);
     print_k_largest_elements(input, 3);
 }
